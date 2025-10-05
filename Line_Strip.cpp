30432b31859ae10427920acb391a4059b1ca70b5
Kunal Pandey <10c.kunalpandey@gmail.com>
2025-10-05 15:20:53 +0530

    Max dist. covered before a gas station

---
+++ Line_Strip.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t ;
+    while(t){
+        int n,x;
+        cin >> n >> x;
+        int arr[n+1];
+        arr[0] = 0;
+        for(int i = 1; i<n+1 ; i++){
+            cin >> arr[i];
+        }
+        long long max_dist = 2*(x-arr[n]);
+        for(int i=0 ; i<n ; i++){
+            if(arr[i+1]-arr[i]>max_dist){
+                max_dist=arr[i+1]-arr[i];
+            }
+        }
+        cout << max_dist << endl;
+
+        t--;
+    }
+}
