f1d777e47b99055d3a19fee61cb345fb6031c191
Kunal Pandey <10c.kunalpandey@gmail.com>
2025-10-17 23:23:17 +0530

    the logic of A.P. worked

---
+++ Twin_Permutations.cpp
@@ -0,0 +1,19 @@
+//  https://codeforces.com/problemset/problem/1831/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int a[n];
+        for( int i = 0 ; i < n ; i++){
+            cin >> a[i];
+            cout << n+1 - a[i] << " " ;
+        }
+        cout << "\n" ;
+    }
+}
